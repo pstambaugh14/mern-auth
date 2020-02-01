747d7eb944c3b65b0e8aeaaa2a7c3bbcaf848d2c
Patricki <pstambaugh14@gmail.com>
2020-02-01 18:18:34 -0500

    Update pod-check.sh

--- pod-check.sh
+++ pod-check.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+su patrick
 pod_exists=`kubectl get pods -n development | grep -i mern-auth | awk '{ print $1 }'`
 
-if [ "$pod_exists" = "mern-auth" ]
-then
+if [ "$pod_exists" != "mern-auth" ]
+then 
+  echo "Pod does not Exist, Will Create Pod..."
+else
   echo "Pod Already Exists, Deleting Previous Pod..."
   kubectl delete pod mern-auth -n development
-else
-  echo "Pod does not Exist, Will Create Pod..."
 fi
