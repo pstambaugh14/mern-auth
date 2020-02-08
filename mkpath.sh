ed08736db2ea73b59e4007a70118aeb523e97586
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-02-08 14:19:05 -0500

    aaalrightty

--- mkpath.sh
+++ mkpath.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-'MINI1'=`whereis minikube`
-'MINI2'=`echo '$MINI1' | awk '{ print $2 }' | sed 's/minikube//g'`
-echo '$MINI2'
+MINI1=`whereis minikube`
+MINI2=`echo $MINI1 | awk '{ print $2 }' | sed 's/minikube//g'`
+MINI3="$MINI2"
+echo $MINI2
--- service-ip.sh
+++ service-ip.sh
@@ -3,5 +3,6 @@
 #exec $MINI2/minikube service list | grep -i "${feSvcName}" | awk '{ print $6 }'  > "${feSvcName}"
 #sh ("${path2} service list | grep -i ${feSvcName} | awk '{ print "${6}" }'")
 #'"${MK_HOME}"/minikube service list | grep -i "${feSvcName}" | awk '{ print "\$6" }'  > "${feSvcName}"'
-'$MINI2/minikube service list | grep -i "${feSvcName}" | awk '{ print /$6 }''
+source ./mkpath.sh
+$MINI3/minikube service list | grep -i "${feSvcName}" | awk '{ print $6 }'
 #> "${feSvcName}"
