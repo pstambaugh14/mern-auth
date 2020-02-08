6f6b091a80899d73497aa99719dc1c90d6816872
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-02-08 14:12:55 -0500

    slowly but steadily

--- mkpath.sh
+++ mkpath.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-MINI1=`whereis minikube`
-MINI2=`echo $MINI1 | awk '{ print $2 }' | sed 's/minikube//g'`
-export $MINI2
-
-verify=`echo $MINI2`
-if $verify="/usr/local/bin/minikube"
-then 'export $MINI2'
-else
-  break;;
-fi
+'MINI1'=`whereis minikube`
+'MINI2'=`echo '$MINI1' | awk '{ print $2 }' | sed 's/minikube//g'`
+echo '$MINI2'
