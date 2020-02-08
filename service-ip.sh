d0b1c063f733f0417212f0629456d5277e4120c2
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-02-08 18:54:32 -0500

    next... creds

--- service-ip.sh
+++ service-ip.sh
@@ -3,7 +3,8 @@
 #exec $MINI2/minikube service list | grep -i "${feSvcName}" | awk '{ print $6 }'  > "${feSvcName}"
 #sh ("${path2} service list | grep -i ${feSvcName} | awk '{ print "${6}" }'")
 #'"${MK_HOME}"/minikube service list | grep -i "${feSvcName}" | awk '{ print "\$6" }'  > "${feSvcName}"'
-source ./mkpath.sh
+
+#source ./mkpath.sh
 
 #$MINI3/minikube service list | grep -i "${feSvcName}" | awk '{ print $6 }'
 
