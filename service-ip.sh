bd8889deed310588f59f8e99c210ec28adaf6130
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-02-08 04:30:57 -0500

    re-try service-ip.sh

--- service-ip.sh
+++ service-ip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-minikube service list | grep -i mern-auth-service | awk '{ print $6 }'
+$path2/minikube service list | grep -i $fwSvcName | awk '{ print $6 }'
+#sh ("${path2} service list | grep -i ${feSvcName} | awk '{ print "${6}" }'")
