0a40f1d5c144c26c5fca9f57ede720e7759c0243
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-02-08 05:38:57 -0500

    bin/sh

--- mkpath.sh
+++ mkpath.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PATH1=`whereis minikube`
-PATH2=`echo $PATH1| awk '{ print $2 }'`
+PATH2=`echo $PATH1 | awk '{ print $2 }'`
 echo "$PATH2"
--- service-ip.sh
+++ service-ip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH2=$PATH2
-$PATH2 service list | grep -i mern-auth-service | awk '{ print $6 }'
+#export PATH2=$PATH2
+/bin/sh -xe $PATH2 service list | grep -i mern-auth-service | awk '{ print $6 }'
 #sh ("${path2} service list | grep -i ${feSvcName} | awk '{ print "${6}" }'")
