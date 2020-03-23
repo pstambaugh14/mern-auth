fd5bbb514e59f28e819a3916242d2e4e618e7418
Patricki <pstambaugh14@gmail.com>
2020-03-23 05:08:42 -0400

    Update configMap_create.sh

--- config/configMap_create.sh
+++ config/configMap_create.sh
@@ -2,5 +2,5 @@
 #kubectl -n development create configmap keys-config --from-file=../../config/keys.js
 #kubectl -n development create configmap keys-config --from-file=keys.js
 #kubectl -n development create configmap keys-config --from-file=keys.js=keys --from-file=passport=passport.js
-kubectl -n development create configmap keys-config --from-file=keys.js --from-file=keys.js
-
+#kubectl -n development create configmap keys-config --from-file=keys.js --from-file=keys.js
+kubectl -n development create configmap keys-config --from-file=keys.js
